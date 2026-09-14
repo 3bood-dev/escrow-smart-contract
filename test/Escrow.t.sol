e3e863a18356988f849223cf90dfaa0bf8ee8cd3
3bood-dev <abdallah.abdelwahab377@gmail.com>
2026-09-14 12:41:28 +0300

    Adding Escrow.t.sol body

---
+++ test/Escrow.t.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test} from "forge-std/Test.sol";
+import {Escrow} from "../src/Escrow.sol";
+
+contract EscrowTest is Test {
+    Escrow public escrow;
+
+    // function setUp() public {
+    //     escrow = new Escrow();
+    // }
+
+    
+}
