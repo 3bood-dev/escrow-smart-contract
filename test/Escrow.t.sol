7235269e3974ef95c25296ae9a7901049d087921
3bood-dev <abdallah.abdelwahab377@gmail.com>
2026-09-15 07:27:51 +0300

    testing Construcor

--- test/Escrow.t.sol
+++ test/Escrow.t.sol
@@ -8,12 +8,22 @@ contract EscrowTest is Test {
     Escrow public escrow;
     address public buyer = makeAddr("buyer");
     address public seller = makeAddr("seller");
-    address public arbitur = makeAddr("arbitur");
+    address public arbiter = makeAddr("arbier");
 
     uint256 public AMOUNT = 1 ether;
     function setUp() public {
-        escrow = new Escrow{value: AMOUNT}(buyer, seller, arbitur);
+        escrow = new Escrow{value: AMOUNT}(buyer, seller, arbiter);
     }
 
+    function test_Constructor()public view{
+        vm.assertEq(escrow.buyer(), buyer);
+        vm.assertEq(escrow.seller(), seller);
+        vm.assertEq(escrow.arbiter(), arbiter);
+        vm.assertEq(escrow.amount(), AMOUNT);
+        vm.assertEq(address(escrow).balance, AMOUNT);
+        vm.assertFalse(escrow.buyerApproved());
+        vm.assertFalse(escrow.sellerApproved());
+        vm.assertFalse(escrow.isDisputeRaised());
+    }
     
 }
